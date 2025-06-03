4d271b59f3e34978bd5c37b056a72546ae7731ec
bestiadivina147 <diegopc147@gmail.com>
2025-06-03 10:38:01 +0200

    Initial commit

---
+++ src/main/java/edu/badpals/bobesponjau4/BobEsponjaU4Application.java
@@ -0,0 +1,13 @@
+package edu.badpals.bobesponjau4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobEsponjaU4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobEsponjaU4Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/badpals/bobesponjau4/BobEsponjaU4ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.badpals.bobesponjau4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobEsponjaU4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
